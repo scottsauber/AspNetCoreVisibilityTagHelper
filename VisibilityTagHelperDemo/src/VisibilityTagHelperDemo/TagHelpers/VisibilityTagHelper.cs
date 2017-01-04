8bd89a5c601adc80a361faf43d13179064b79230
Scott Sauber <stsauber@gmail.com>
2017-01-03 20:36:17 -0600

    Add Visibility Tag Helper

---
+++ VisibilityTagHelperDemo/src/VisibilityTagHelperDemo/TagHelpers/VisibilityTagHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace VisibilityTagHelperDemo.TagHelpers
+{
+    [HtmlTargetElement("div")]
+    public class VisibilityTagHelper : TagHelper
+    {
+        // default to true otherwise all existing target elements will not be shown, because bool's default to false
+        public bool IsVisible { get; set; } = true;
+        
+        public override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            if (!IsVisible)
+                output.SuppressOutput();
+
+            return base.ProcessAsync(context, output);
+        }
+    }
+}
